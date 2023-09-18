deeeac289a5b0de0e84ceed69287c74a6a25042a
Simon Erkelens <github@casa-laguna.net>
2023-09-18 21:27:29 +1200

    Rename the SCSS files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-mix.sass('client/css/nativeads.js.scss', 'dist/css/nativeads.js.css');
+mix.sass('client/css/base.scss', 'dist/css/nativeads.js.css');
 mix.js('client/js/base.js', 'dist/js/base.js');
 
 mix.webpackConfig({
