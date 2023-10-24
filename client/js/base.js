1ead58de61bc31847b4a7468eb2707d77bc8526a
Simon Erkelens <github@casa-laguna.net>
2023-10-24 23:02:15 +1300

    Update to only run on screens larger than 620px

--- client/js/base.js
+++ client/js/base.js
@@ -1,13 +1,15 @@
-const advert = document.getElementById('adblock');
-const hideClasses = [
-    'hidden',
-    'd-none',
-    'invisible',
-    'dontshow',
-    'noshow'
-]
-if (advert) {
-    hideClasses.forEach(hideClass => {
-        advert.classList.remove(hideClass)
-    });
+if (window.matchMedia('(max-width: 620px)').matches) {
+    const advert = document.getElementById('adblock');
+    const hideClasses = [
+        'hidden',
+        'd-none',
+        'invisible',
+        'dontshow',
+        'noshow'
+    ]
+    if (advert) {
+        hideClasses.forEach(hideClass => {
+            advert.classList.remove(hideClass)
+        });
+    }
 }
